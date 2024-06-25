126514ef1108b418ebbccf4d56ff5734e8eb586a
brepowell <85724630+brepowell@users.noreply.github.com>
2024-06-25 15:39:30 -0600

    Re-arranged some of the code in icesat2 practice code

--- practice_plotting/icesat2_data_practice_visualization.py
+++ practice_plotting/icesat2_data_practice_visualization.py
@@ -35,13 +35,13 @@ from e3sm_data_practice_visualization import *
     # ATL06 is Land Ice Height
     # ATL10 is Sea Ice Freeboard ** what we are interested in **
 
-PRODUCTS = {                  # Some algorithms are not compatible
-    "ATL07": ["Sea Ice Height"],
-    "ATL06": ["Land Ice Height"],
-    "ATL10": ["Sea Ice Freeboard"]
-}
-
 SHORT_NAME = 'ATL10' # Change this if you want to do different queries
+
+PRODUCTS = {
+    "ATL07": "Sea Ice Height",
+    "ATL06": "Land Ice Height",
+    "ATL10": "Sea Ice Freeboard"
+}
 
 # spatial_extent examples, using a rectangle, polygon, or a shape file:
     # Decimal degrees for the lower left longitude, lower left latitude, 
@@ -63,37 +63,41 @@ LOWER_LEFT_LATITUDE     = 50
 UPPER_RIGHT_LONGITUDE   = -180
 UPPER_RIGHT_LATITUDE    = 90
 
-def getSubsetOfSatData():
+def printDetails(region):
+    """ This helps you narrow down your query to a good range. 
+    Ideally, you do not want to plot a lot of tracks. """
+    print("Queried for ", PRODUCTS[region.product])
+    print("Spatial Extent: ", region.spatial_extent[1])
+    print("Date Range:     ", region.dates)
+    print("Start Time:     ", region.start_time)
+    print("End Time:       ", region.end_time)
+    print("Version:        ", region.product_version)
+    print("Cycles:         ", list(set(region.avail_granules(cycles=True)[0]))) #region.cycles
+    print("Tracks:         ", list(set(region.avail_granules(tracks=True)[0]))) #region.tracks
 
+def getSubsetOfSatData():
+    """ Query for only a small subset of the data, using the time constraints to narrow it down. """
     spatial_extent  = [LOWER_LEFT_LONGITUDE, LOWER_LEFT_LATITUDE, UPPER_RIGHT_LONGITUDE, UPPER_RIGHT_LATITUDE]
     # tracks. See https://icesat-2.gsfc.nasa.gov/science/specs
 
     date_range      = ['2020-7-1', '2020-7-1'] # YYYY-MM-DD
-    start_time      = '15:00:00'
+    start_time      = '17:00:00'
     end_time        = '20:00:00'
 
     # Use the parameters that you specified to query the database for that area in that date range
     region = ipx.Query(SHORT_NAME, spatial_extent, date_range, start_time, end_time)
+
+    printDetails(region)
     return region
 
-def printDetails(region):
-
-    print("Queried for ", PRODUCTS[region.product][0])
-    print("Date Range: ", region.dates)
-    print("Start Time: ", region.start_time)
-    print("End Time:   ", region.end_time)
-    print("Version:    ", region.product_version)
-    print("Cycles:     ", list(set(region.avail_granules(cycles=True)[0]))) #region.cycles
-    print("Tracks:     ", list(set(region.avail_granules(tracks=True)[0]))) #region.tracks
 
 def downloadSatelliteData(region):
-    #path = './satellite_data'
-    #region.download_granules(path)
-    region.order_granules(format='NetCDF4-CF')
+    path = './satellite_data'
+    region.order_granules(path=path, format='NetCDF4-CF')
 
 def main():
     region = getSubsetOfSatData()
-    printDetails(region)
+    # downloadSatelliteData(region)
 
 if __name__ == "__main__":
     main()
