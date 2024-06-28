6659d01eca68aaa7e234208141d7b5fb83022446
brepowell <85724630+brepowell@users.noreply.github.com>
2024-06-28 15:00:01 -0600

    Looked at available variables for subsetting the data.

--- practice_plotting/icesat2_data_practice_visualization.py
+++ practice_plotting/icesat2_data_practice_visualization.py
@@ -15,7 +15,26 @@
 # Have e3sm-data-practice-visualization.py in the same directory
 
 import icepyx as ipx
+import pprint # Pretty print library to print the variables nicely
 from e3sm_data_practice_visualization import *
+
+######################
+# ALL ABOUT ICESAT-2 #
+######################
+
+    # Basic info about IceSat-2:    https://nsidc.org/data/icesat-2
+    # More info about the data:     https://openaltimetry.earthdatacloud.nasa.gov/data/
+
+    # Interactive site to look at the data: https://openaltimetry.earthdatacloud.nasa.gov/data/icesat2/
+    # You can see all the possible tracks to select per day.
+    # Example from that site, when looking at Sea Ice Freeboard
+    # PHOTON DATA FOR SEGMENT 323566
+    # Latitude: 83.821, Longitude: -100.921, Elevation: 0.0955489
+    # Time: 2019-12-26 00:02:45.943, Track ID: 1376, Beam: gt2r
+
+    # Example Visuals: 
+    # https://svs.gsfc.nasa.gov/4734/ 
+    # https://svs.gsfc.nasa.gov/4373/ Shows the orbit
 
 #################################
 # VARIABLES FOR AN ICESAT QUERY #
@@ -31,16 +50,13 @@ from e3sm_data_practice_visualization import *
 
 # SHORT_NAME variable possibilities: 
 # https://nsidc.org/data/icesat-2/products
-    # ATL07 is Sea Ice Height
-    # ATL06 is Land Ice Height
-    # ATL10 is Sea Ice Freeboard ** what we are interested in **
-
+    
 SHORT_NAME = 'ATL10' # Change this if you want to do different queries
 
 PRODUCTS = {
-    "ATL07": "Sea Ice Height",
-    "ATL06": "Land Ice Height",
-    "ATL10": "Sea Ice Freeboard"
+    "ATL07": "Sea Ice Height",      # ATL07 is Sea Ice Height
+    "ATL06": "Land Ice Height",     # ATL06 is Land Ice Height
+    "ATL10": "Sea Ice Freeboard"    # ATL10 is Sea Ice Freeboard ** what we are interested in **
 }
 
 # spatial_extent examples, using a rectangle, polygon, or a shape file:
@@ -58,12 +74,17 @@ PRODUCTS = {
     # polygon_file_spatial_extent = './supporting_files/data-access_PineIsland/glims_polygons.shp'
 
 # Bounding Box
-LOWER_LEFT_LONGITUDE    = 180
+# Originally, I thought 180, 50, -180, 90 
+# would give me a good view, but after playing 
+# with the interactive OpenAltimetry site, the following gets a good bounding box
+LOWER_LEFT_LONGITUDE    = -80
 LOWER_LEFT_LATITUDE     = 50
-UPPER_RIGHT_LONGITUDE   = -180
-UPPER_RIGHT_LATITUDE    = 90
+UPPER_RIGHT_LONGITUDE   = 110
+UPPER_RIGHT_LATITUDE    = 80
 
-def printDetails(region):
+TRACKS = ['0977']     # See https://icesat-2.gsfc.nasa.gov/science/specs
+
+def printQueryDetails(region):
     """ This helps you narrow down your query to a good range. 
     Ideally, you do not want to plot a lot of tracks. """
     print("Queried for ",     PRODUCTS[region.product])
@@ -75,29 +96,60 @@ def printDetails(region):
     print("Cycles:         ", list(set(region.avail_granules(cycles=True)[0]))) #region.cycles
     print("Tracks:         ", list(set(region.avail_granules(tracks=True)[0]))) #region.tracks
 
-def getSubsetOfSatData():
-    """ Query for only a small subset of the data, using the time constraints to narrow it down. """
-    spatial_extent  = [LOWER_LEFT_LONGITUDE, LOWER_LEFT_LATITUDE, UPPER_RIGHT_LONGITUDE, UPPER_RIGHT_LATITUDE]
-    # tracks. See https://icesat-2.gsfc.nasa.gov/science/specs
+def getSubsetOfSatData(print=True):
 
-    date_range      = ['2020-7-1', '2020-7-1'] # YYYY-MM-DD
-    start_time      = '17:00:00'
+    """ Query for only a small subset of the data, using the time constraints to narrow it down. 
+    ICESat-2 lauched on September 15, 2018, so do not expect to be able to see data earlier than that. """
+    spatial_extent  = [LOWER_LEFT_LONGITUDE, LOWER_LEFT_LATITUDE, UPPER_RIGHT_LONGITUDE, UPPER_RIGHT_LATITUDE]
+    date_range      = ['2020-2-28', '2020-2-29'] # YYYY-MM-DD
+    start_time      = '15:00:00'
     end_time        = '20:00:00'
 
     # Use the parameters that you specified to query the database for that area in that date range
-    region = ipx.Query(SHORT_NAME, spatial_extent, date_range, start_time, end_time)
+    region = ipx.Query(SHORT_NAME, spatial_extent, date_range, start_time, end_time, tracks=TRACKS)
+    # region = ipx.Query(SHORT_NAME, spatial_extent, date_range) # Use default start and end times.
 
-    printDetails(region)
+    # Hard-Coded Example
+    # region = ipx.Query('ATL06',[-55, 68, -48, 71],['2019-02-22','2019-02-28'], start_time='00:00:00', end_time='23:59:59')
+
+    if print==True:
+        printQueryDetails(region)
+
     return region
 
-# https://icepyx.readthedocs.io/en/latest/example_notebooks/IS2_data_access2-subsetting.html
+def seeAvailableVariables(region):
+    import sys, io
+
+    filename = "variables.txt"
+    with open(filename, 'w') as file:
+        # Create a string buffer to capture the printed output
+        buffer = io.StringIO()
+        # Redirect standard output to the buffer
+        sys.stdout = buffer
+        
+        # Call the function that prints its output
+        region.show_custom_options(dictview=True)
+        
+        # Restore the standard output
+        sys.stdout = sys.__stdout__
+        
+        # Get the content of the buffer
+        output = buffer.getvalue()
+        
+        if output:
+            file.write(output)
+        else:
+            print("Error: The output of show_custom_options is None or empty.")
+
+# https://icepyx.readthedocs.io/en/latest/example_notebooks/IS2_data_access2-subsetting.html#why-does-the-subsetter-say-no-matching-data-was-found
 def downloadSatelliteData(region):
     path = './satellite_data'
     region.download_granules(path=path, format='NetCDF4-CF') # Calls order_granules() under the hood
 
 def main():
-    region = getSubsetOfSatData()
-    downloadSatelliteData(region)
+    region = getSubsetOfSatData() # Can set print to be False
+    # seeAvailableVariables(region) # Saves the big long list of variables into variables.txt file.
+    # downloadSatelliteData(region)
 
 if __name__ == "__main__":
     main()
