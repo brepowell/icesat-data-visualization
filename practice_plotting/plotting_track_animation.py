7ec0e8eb8ee05d515ee0149659f97e5ff46c502f
brepowell <85724630+brepowell@users.noreply.github.com>
2024-07-15 15:23:15 -0600

    North pole track works for plotting_track_animation

--- practice_plotting/plotting_track_animation.py
+++ practice_plotting/plotting_track_animation.py
@@ -105,9 +105,51 @@ def plotNorthAndSouthTrackAnimation():
     print("Saved .gif file")
 
 
+def plotNorthPoleTrackAnimation():
+    
+    # Load the data to plot
+    output              = loadData(runDir, outputFileName)
+    latCell, lonCell    = getLatLon(output)
+    timeCell            = getTime(output, len(lonCell))
+
+    variableToPlot1Day  = reduceToOneDay(output, keyVariableToPlot=VARIABLETOPLOT)
+
+    # Downsample the data
+    factor = 100  # Downsampling factor
+    latCell, lonCell, timeCell, variableToPlot1Day = downsampleData(latCell, lonCell, timeCell, variableToPlot1Day, factor)
+
+    # Plot the north pole
+    fig, northMap = generateNorthPoleAxes()
+    scatterNorth = generateNorthPoleMap(fig, northMap, latCell, lonCell, variableToPlot1Day, mapImageFileName, 1,1,1,1,1,5.0)
+    
+    # Initialize the text box
+    textBox = northMap.text(0.05, 0.95, "", transform=northMap.transAxes, fontsize=14, verticalalignment='top', bbox=boxStyling)
+
+    def update(frame):
+        # Update the data stored on each artist for each frame
+        scatterNorth.set_offsets(np.c_[lonCell[:frame], latCell[:frame]])
+
+        # Update the text box with the current time
+        textBoxString = "Time: " + str(convertTime(timeCell[frame]))
+        textBox.set_text(textBoxString)
+
+        return scatterNorth, textBox
+
+    print("Generated .png file")
+
+    startTime = time.time()
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=latCell.size, interval=1)
+    ani.save(filename=animationFileName, writer="pillow")
+    endTime = time.time()
+    print("It took this much time: ", endTime-startTime)
+
+    # =======================
+    print("Saved .gif file")
+
+
 def main():
-    plotNorthAndSouthTrackAnimation()
-    # plotNorthPoleTrackAnimation()
+    # plotNorthAndSouthTrackAnimation()
+    plotNorthPoleTrackAnimation()
 
 if __name__ == "__main__":
     main()
