868505ba515feaa4b8260aa0b9f399d311f0f7f3
brepowell <85724630+brepowell@users.noreply.github.com>
2024-07-19 14:44:06 -0600

    Pulls ice and snow volume and ice area from a E3SM daily.nc file

--- practice_plotting/make_a_netCDF_file.py
+++ practice_plotting/make_a_netCDF_file.py
@@ -1,7 +1,7 @@
 # Breanna Powell
 # Created: 07/17/2024
 
-# This is a great tutorial about writing netCDF files:
+# See this great tutorial about writing netCDF files:
 # https://unidata.github.io/python-training/workshop/Bonus/netcdf-writing/
 
 import numpy as np
@@ -10,12 +10,21 @@ from datetime import datetime
 import os
 from utility import *
 
-USER        = os. getlogin()                        #TODO: check if this is ok
-SOURCE      = "SOME PATH NAME TO FILL IN LATER"     #TODO: make this dynamic
-CELLCOUNT   = 233365 #236853                        #TODO: Depends on mesh size
-FILL_VALUE = fill_value = -99999.0
+USER                = os. getlogin()                        #TODO: check if this is ok for Perlmutter
+SOURCE              = "SOME PATH NAME TO FILL IN LATER"     #TODO: make this dynamic
+NETCDF_FILE_NAME    = "new.nc"                              #TODO: make this dynamic
+CELLCOUNT           = 233365 #236853                        #TODO: make this dynamic
+
+FILL_VALUE      = -99999.0
+DENSITY_WATER   = 1026
+DENSITY_ICE     = 917
+DENSITY_SNOW    = 330
 
 #TODO: Make sure the data types are correct; there should be an "f" after many of them.
+
+########################
+# OPEN THE NETCDF FILE #
+########################
 
 try: ncfile.close()  # just to be safe, make sure dataset is not already open.
 except: pass
@@ -26,14 +35,15 @@ ncfile = Dataset('new.nc',mode='w',format='NETCDF4_CLASSIC')
 ##############
 
 # Create the dimensions (nCells is the only dimension needed)
-nCells = ncfile.createDimension('nCells', CELLCOUNT)     # latitude axis
-
-now = datetime.now()
-historyString = now.strftime("%d-%B-%Y %H:%M:%S") + ": File created by " + USER
+nCells = ncfile.createDimension('nCells', CELLCOUNT)
 
 ##############
 # ATTRIBUTES #
 ##############
+
+# Grab the date for the header information
+now = datetime.now()
+historyString = now.strftime("%d-%B-%Y %H:%M:%S") + ": File created by " + USER
 
 # Set the attributes
 ncfile.title        = "Comparison of icesat freeboard with E3SM"
@@ -72,24 +82,22 @@ stdmf   = createVariableForNetCDF("stdmf", "model freeboard standard deviation",
 stdof   = createVariableForNetCDF("stdof", "observed freeboard standard deviation", 
                         vmax = 0.9629242, vmin = 0.01656876, fillvalue = FILL_VALUE)
 
-########################
-# STATISTICAL ANALYSIS #
-########################
+################
+# READ IN DATA #
+################
 
-latCell, lonCell    = loadMesh(runDir, meshFileName)
 satelliteFileName   = r"\satellite_data_preprocessed\one_day\icesat_E3SM_spring_2008_02_22_16.nc"
 satelliteData       = loadData(runDir, satelliteFileName)
-freeBoardReadings   = reduceToOneDay(satelliteData, keyVariableToPlot=VARIABLETOPLOT)
+freeBoardReadings   = reduceToOneDay(satelliteData, keyVariableToPlot="freeboard")
 cellIndicesForAllSamples      = reduceToOneDay(satelliteData, "modcell")
 cellIndicesForAllObservations = returnCellIndices(satelliteData)
 
-print("Shape of mesh:                          ", latCell.shape)
 print("Shape of freeBoardReadings:             ", freeBoardReadings.shape)
 print("Shape of cellIndicesForAllSamples:      ", cellIndicesForAllSamples.shape)
 print("Shape of cellIndicesForAllObservations: ", cellIndicesForAllObservations.shape)
 
 ############################
-# SATELLITE ONLY VARIABLES #
+# SATELLITE-ONLY VARIABLES #
 ############################
 # These variables are easy to pull directly from the satellite data.
 
@@ -103,40 +111,64 @@ sampleof[:] = np.bincount(cellIndicesForAllObservations) # Collect all photon co
 # Observed freeboard mean is the sum of all photon readings 
 # in that cell over all time / sampleof
 meanof[:] = np.full(meanof.shape, FILL_VALUE)
-reached = 0
 for cellIndex in range(CELLCOUNT):
     # Find all the indices that contain freeboard data
     freeBoardIndices = np.where(cellIndicesForAllObservations == cellIndex)[0]
     if len(freeBoardIndices) > 0:
         meanValue = np.mean(freeBoardReadings[freeBoardIndices])
-        reached +=1
         meanof[cellIndex] = meanValue 
 
-print(reached)
+# Observed freeboard mean is the sum of all photon readings 
+# in that cell over all time / sampleof
+stdof[:] = np.full(stdof.shape, FILL_VALUE)
+for cellIndex in range(CELLCOUNT):
+    # Find all the indices that contain freeboard data
+    freeBoardIndices = np.where(cellIndicesForAllObservations == cellIndex)[0]
+    if len(freeBoardIndices) > 0:
+        standardDeviation = np.std(freeBoardReadings[freeBoardIndices])
+        stdof[cellIndex] = standardDeviation 
 
+###################
+# MODEL VARIABLES #
+###################
 
+modelDailyDataFile  = r"\output_files\Breanna_D_test_1x05_days.mpassi.hist.am.timeSeriesStatsDaily.0001-01-01.nc"
+modelData           = loadData(runDir, modelDailyDataFile)
+snowVolumeCells     = reduceToOneDay(modelData, keyVariableToPlot="timeDaily_avg_snowVolumeCell") 
+iceVolumeCells      = reduceToOneDay(modelData, keyVariableToPlot="timeDaily_avg_iceVolumeCell")
+iceAreaCells        = reduceToOneDay(modelData, keyVariableToPlot="timeDaily_avg_iceAreaCell")
 
+print(snowVolumeCells.shape)
+print(iceVolumeCells.shape)
+print(iceAreaCells.shape)
+
+# TODO: Future work could be to add the Freeboard variable to E3SM's variables
+
+def getFreeboard(heightIce, heightWater, heightSnow):
+    """Formula to calculate freeboard: hf = hi (pw-pi)/pw + hs (pw-ps)/pw.
+    Where p means density; h is height, w is water, i is ice, s is snow"""
+    return heightIce*(DENSITY_WATER-DENSITY_ICE)/DENSITY_WATER + heightSnow*(DENSITY_WATER-DENSITY_SNOW)/DENSITY_WATER
+
+def getThickness(volume, area):
+    """ volume / area of cell = height (thickness) """
+    return volume / area
 # Model freeboard mean is 
-#meanmf = sum of the meanof for that cell over all time / samplemf
-#=> hf = hi (ρw-ρi)/ρw + hs (ρw-ρs)/ρw
 
-#######################################
-# CALCULATE FREEBOARD FROM MODEL DATA #
-#######################################
-# TODO: Future work could be to add this variable to E3SM's variables
-
-# E3SM ice volume / E3SM ice area of cell = ice thickness
+iceHeightCells  = getThickness(iceVolumeCells, iceAreaCells)
+snowHeightCells = getThickness(snowVolumeCells, iceAreaCells)
+print("Ice Height Cells",  iceHeightCells.shape)
+print("Snow Height Cells", snowHeightCells.shape)
 
 # height ice + height snow = height water + height of freeboard
-
 # freeboard = ice thickness + snow height - water height (which is 0?)
 
-
-
 # Read data back from variable, print min and max
-print("-- Min/Max values:", samplemf[:].min(), samplemf[:].max())
-print("-- Min/Max values:", sampleof[:].min(), sampleof[:].max())
-print("-- Min/Max values:", meanof[:].min(),   meanof[:].max())
+print("===== SATELLITE VARIABLES ======")
+print("Samplemf Min/Max values:", samplemf[:].min(), samplemf[:].max())
+print("Sampleof Min/Max values:", sampleof[:].min(), sampleof[:].max())
+print("Meanof   Min/Max values:", meanof[:].min(),   meanof[:].max())
+print("Stdof    Min/Max values:", stdof[:].min(),    stdof[:].max())
+print("\n=====   MODEL VARIABLES   ======")
 
 # close the Dataset
 ncfile.close()
--- practice_plotting/print_nc_file_info.py
+++ practice_plotting/print_nc_file_info.py
@@ -2,23 +2,23 @@ from utility import *
 
 runDir         = os.path.dirname(os.path.abspath(__file__))       # Get current directory path
 
-outputFileName   = r"\mesh_files\mpassi.IcoswISC30E3r5.20231120.nc"
+# fileName   = r"\mesh_files\mpassi.IcoswISC30E3r5.20231120.nc"
 
 # Checking the sizes for the number of grid cells per .nc file
-# outputFileName = r"\mesh_files\DECK_Coast.nc"                     # Size in grid cells: 18157
-# outputFileName = r"\mesh_files\E3SM_DECK_Emulator_File.nc"        # Size in grid cells: 235160
-# outputFileName = r"\mesh_files\E3SM_V1_C_grid_Coast.nc"           # Size in grid cells: 74686
-# outputFileName = r"\mesh_files\ICESat_Masked_E3SM.nc"             # Size in grid cells: 6220
+# fileName = r"\mesh_files\DECK_Coast.nc"                     # Size in grid cells: 18157
+# fileName = r"\mesh_files\E3SM_DECK_Emulator_File.nc"        # Size in grid cells: 235160
+# fileName = r"\mesh_files\E3SM_V1_C_grid_Coast.nc"           # Size in grid cells: 74686
+# fileName = r"\mesh_files\ICESat_Masked_E3SM.nc"             # Size in grid cells: 6220
 
 # FILES FOR 5 OR 10 DAY SIMULATION:
-# outputFileName = r"\mesh_files\seaice.EC30to60E2r2.210210.nc"     # Size in grid cells: 236853
-#outputFileName = r"\output_files\Breanna_D_test_1x05_days.mpassi.hist.am.timeSeriesStatsDaily.0001-01-01.nc" # 236853
+# fileName = r"\mesh_files\seaice.EC30to60E2r2.210210.nc"     # Size in grid cells: 236853
+fileName = r"\output_files\Breanna_D_test_1x05_days.mpassi.hist.am.timeSeriesStatsDaily.0001-01-01.nc" # 236853
 
 # FILES FOR 1 YEAR SIMULATION:
-# outputFileName = r"\mesh_files\mpassi.IcoswISC30E3r5.20231120.nc" # Size in grid cells:     465044
+# fileName = r"\mesh_files\mpassi.IcoswISC30E3r5.20231120.nc" # Size in grid cells:     465044
 
 # FILES FOR SATELLITE TRACK ANIMATION:
-#outputFileName = r"\satellite_data_preprocessed\one_day\icesat_E3SM_spring_2008_02_22_16.nc"  # 6533
+#fileName = r"\satellite_data_preprocessed\one_day\icesat_E3SM_spring_2008_02_22_16.nc"  # 6533
 
-output = loadData(runDir, outputFileName)
+output = loadData(runDir, fileName)
 printAllAvailableVariables(output)
