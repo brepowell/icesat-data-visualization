1836fd980c590d38d4fa679abbbb2e13364f8610
Breanna Powell <brelypo@login25.chn.perlmutter.nersc.gov>
2024-07-26 13:45:19 -0700

    printed out dates for 2003-02-01.nc file

--- practice_plotting/print_nc_file_dates.py
+++ practice_plotting/print_nc_file_dates.py
@@ -6,8 +6,8 @@ runDir = perlmutterpathDailyData
 
 
 #fileName = r"/output_files/Breanna_D_test_5_nodes_1_nyears_with_fewer_nodes.mpassi.hist.am.timeSeriesStatsDaily.0001-03-01.nc"
-fileName = r"v3.LR.historical_0051.mpassi.hist.am.timeSeriesStatsDaily.2008-02-01.nc"
-
+#fileName = r"v3.LR.historical_0051.mpassi.hist.am.timeSeriesStatsDaily.2008-02-01.nc"
+fileName = r"v3.LR.historical_0051.mpassi.hist.am.timeSeriesStatsDaily.2003-02-01.nc"
 
 output = loadData(runDir, fileName)
 days = getNumberOfDays(output)
