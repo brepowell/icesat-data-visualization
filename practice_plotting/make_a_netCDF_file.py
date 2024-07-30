13648dbc405a4eaf0ab508d0f050d7b3bcbaff9b
brepowell <85724630+brepowell@users.noreply.github.com>
2024-07-30 10:32:38 -0600

    added searching for

--- practice_plotting/make_a_netCDF_file.py
+++ practice_plotting/make_a_netCDF_file.py
@@ -150,6 +150,7 @@ for fileIndex in range(0, stoppingPoint):
     # Find the file with that name
     filenamePattern = f"icesat_E3SM_*_{year}_{str(month).zfill(2)}_{str(day).zfill(2)}_{str(hour).zfill(2)}.nc"
     searchPattern = os.path.join(runDir, filenamePattern)
+    print("Searching for: ", searchPattern)
     matchingFiles = glob.glob(searchPattern)
     satelliteFileName = matchingFiles[0] if matchingFiles else None
 
