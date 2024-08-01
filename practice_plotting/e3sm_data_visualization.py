0f0b49bb25ade750a7176596011d3f50a4d67896
brepowell <85724630+brepowell@users.noreply.github.com>
2024-08-01 15:59:19 -0600

    calling generateNorthPoleMap with variableToPlot1Day

--- practice_plotting/e3sm_data_visualization.py
+++ practice_plotting/e3sm_data_visualization.py
@@ -226,7 +226,8 @@ def main():
     fig, northMap = generateNorthPoleAxes()
 
     # Plotting with a variable
-    generateNorthPoleMap(fig, northMap, latCell, lonCell, variableToPlot1Day, mapImageFileName, dot_size=0.4)
+    generateNorthPoleMap(fig, northMap, latCell, lonCell, variableToPlot1Day, mapImageFileName)
+    #generateNorthPoleMap(fig, northMap, latCell, lonCell, mapImageFileName, dot_size=0.4)
 
 if __name__ == "__main__":
     main()
