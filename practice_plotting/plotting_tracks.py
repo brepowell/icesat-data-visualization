0e163ee9a34dd762337ee9484a8ee6a8b0563ae6
brepowell <85724630+brepowell@users.noreply.github.com>
2024-08-01 13:57:44 -0600

    change dot size to 0.01

--- practice_plotting/plotting_tracks.py
+++ practice_plotting/plotting_tracks.py
@@ -29,7 +29,7 @@ def generateNorthPoleStaticPlotOfTrackLatLong(fig, northMap, mapImageFileName, g
         print("Satellite file: ", file)
         satelliteData       = loadData("", file) #PM
         latCell, lonCell    = getLatLon(satelliteData)
-        scatter = mapNorthernHemisphere(latCell, lonCell, "Arctic_lat_long", northMap, 0.05)     # Map northern hemisphere
+        scatter = mapNorthernHemisphere(latCell, lonCell, "Arctic_lat_long", northMap, 0.01)     # Map northern hemisphere
 
     plt.suptitle("lat and long", size="x-large", fontweight="bold")
 
