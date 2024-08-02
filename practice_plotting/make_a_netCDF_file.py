067a1f187563ac8f42a96e89074a7fcee5b13b64
brepowell <85724630+brepowell@users.noreply.github.com>
2024-08-02 15:36:09 -0600

    getting shape of timecluster

--- practice_plotting/make_a_netCDF_file.py
+++ practice_plotting/make_a_netCDF_file.py
@@ -125,6 +125,7 @@ def main():
     ########################
     fileCount, timeStrings, timeCluster, timeYear, timeMonth, timeDay, timeHour, timeGregorian = loadSynchronizer()
     print("Number of satellite tracks in Synch file: ", fileCount)
+    print("shape of timeCluster", timeCluster.shape)
     timeCluster = timeCluster[:1].ravel()
     print(timeCluster)
 
