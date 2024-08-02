addbf2bdfb0f8e91eaa76e21fdfcfa7dadfb4855
brepowell <85724630+brepowell@users.noreply.github.com>
2024-08-02 15:47:24 -0600

    timeCluster cast to array

--- practice_plotting/make_a_netCDF_file.py
+++ practice_plotting/make_a_netCDF_file.py
@@ -126,7 +126,7 @@ def main():
     fileCount, timeStrings, timeCluster, timeYear, timeMonth, timeDay, timeHour, timeGregorian = loadSynchronizer()
     print("Number of satellite tracks in Synch file: ", fileCount)
     print("shape of timeCluster", timeCluster.shape)
-    timeClusters = timeCluster[:1]
+    timeClusters = np.array(timeCluster)
     print(timeClusters)
 
     ########################
