0528cc1cc8375c04d7bf4d22c16e02f66668a32e
Breanna Powell <brelypo@login38.chn.perlmutter.nersc.gov>
2024-08-06 12:40:57 -0700

    ran fall and spring 2004 to 2008, but spring 04 and 08 and fall 08 have problems

--- practice_plotting/config.py
+++ practice_plotting/config.py
@@ -29,7 +29,7 @@ LONGITUDEVARIABLE   = "lonCell"   #longitude
 CELLVARIABLE        = "cell"
 
 SEASON              = "fall"
-YEAR                = "2003"
+YEAR                = "2008"
 NEW_NETCDF_FILE_NAME = f"{SEASON}_{YEAR}.nc"
 #NEW_NETCDF_FILE_NAME = "ALL_SATELLITE_DATA.nc"
 
